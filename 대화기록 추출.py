b98cce38c4f0f2586137dd210d6014f1aa5c5077
Sehyun_Kim <79848348+FURY312@users.noreply.github.com>
2024-01-11 17:17:16 +0900

    Add files via upload

---
+++ 대화기록 추출.py
@@ -0,0 +1,14 @@
+#카톡 대화기록 형식파일만 작동함
+
+a='' #추출할 대상의 이름
+
+f = open( 'data_set/KakaoTalkChats.txt' , mode='r' ,encoding='utf-8')
+lines = f.readlines()
+for line in lines:
+    print( line, end='' )   
+    if a in line:
+        main=line.split(' : ')[1]
+        with open("main.txt", "a", encoding='utf-8') as fs:
+            fs.write(main)
+            fs.close()
+f.close()ㄴ
